2e3091d7d828534edbb05b226ec5ce58021afb96
rekuantsun <lqt291102@gmail.com>
2021-11-15 15:50:06 +0700

    EXCERCISE

---
+++ P2-15-11/Date-1511.py
@@ -0,0 +1,33 @@
+#1
+import random
+
+L=[1,2,3,4,5,6]
+print('sum of list:',sum(L))
+
+#2
+import math
+L1=[29,11,2]
+print('multiply all the items in a list:',math.prod(L1))
+
+#3
+print('the largest number from a list:',max(L1))
+
+#4
+print('the smallest number from a list:',min(L1))
+
+#5
+L.sort(reverse=True)
+print('sort a list in descending order: ',L)
+
+#6
+L1.sort()
+print('sort a list in ascending order:',L1)
+
+#7
+print('get the size of a list:',len(L))
+
+#8
+print('get random element(s) of a list:',random.choice(L))
+
+#9
+print('join two different lists into one:',L+L1)
