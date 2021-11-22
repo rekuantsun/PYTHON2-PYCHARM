6a8c97f7472ab9dc720dd5c239c4eb87ec87cc9c
rekuantsun <lqt291102@gmail.com>
2021-11-22 15:54:12 +0700

    Excercise

--- P2-22-11-BT LOOP/B2/no5.py
+++ P2-22-11-BT LOOP/B2/no5.py
@@ -3,8 +3,11 @@
 nhập vào 1 kí tự từ a đến z)
 - Hiển thị danh sách đó ra màn hình
 - Yêu cầu người dùng nhập vào một số nguyên N từ bàn phím'''
+l=[]
 while True:
     n = input('số nguyên nhập vào: ')
     if (n.isalpha()==True):
         break
     else:
+        l.append(int(n))
+print(l)
